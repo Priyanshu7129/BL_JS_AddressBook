7840bc5353e91d4a0e4249d039779e8b4bd53db8
Priyanshu7129 <priyanshu7129@gmail.com>
2025-03-21 12:00:04 +0530

    Created AddressBook and contact class

---
+++ Contact.js
@@ -0,0 +1,22 @@
+class Contact {
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phone;
+    email;
+
+    //create constructor
+    constructor(firstName, lastName, address, city, state, zip, phone, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+}
